c4fabfb4c6712ee4d514154f5d522f8e6dd14f4d
TheDzigner <83682078+TheDzigner@users.noreply.github.com>
2023-04-02 10:29:11 -0400

    Add files via upload

--- anim.js
+++ anim.js
@@ -2,7 +2,7 @@ const openSearchBarBtn =  document.querySelector(".open-search-mobile")
 const searchWordsInput = document.querySelector(".search-words-input")
 const openMenuMobile = document.querySelector(".open-menu-mobile")
 const menu = document.querySelector(".menu")
-
+const title = document.querySelector(".title")
 
 openSearchBarBtn.addEventListener("click",function(e){
    
@@ -10,10 +10,12 @@ openSearchBarBtn.addEventListener("click",function(e){
         e.target.textContent = "search"
         searchWordsInput.classList.remove("active")
         openMenuMobile.classList.remove("inactive")
+        title.style.display = "block"
     } else {
         e.target.textContent = "close"
         searchWordsInput.classList.add("active")
         openMenuMobile.classList.add("inactive")
+        title.style.display = "none"
     }
 })
 
